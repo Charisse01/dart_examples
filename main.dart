7efd07232ee9a4124abda920c41ad52d61a7690c
Charisse01 <chariscastillo21@gmail.com>
2024-10-06 18:03:11 -0400

    I added a second example using Maps

--- main.dart
+++ main.dart
@@ -1,6 +1,29 @@
+/*
+//example 1 using arrow systaxis
 void main() {
   List<int> numbers = [1, 7, 10, 4, 5, 8];
   
   numbers.forEach((num) => print(num * 2));
   
+}
+*/
+
+
+
+//example 2 using maps
+void main() {
+  var books = {
+    'romance': 'El Fabricante de Lagrimas',
+    'horror': 'Carrie',
+    'comedy': 'A Confederacy of Dunces',
+    'fantasy': 'Alas de Hierro',
+    'thriller': 'The Fourth Monkey',
+    'science fiction': 'Dune'
+  };
+
+  print(books['fantasy']);   
+
+  print(books.containsKey('horror')); 
+  
+  print(books.containsValue('Powerless'));  
 }
